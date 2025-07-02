26140e0e82f7eb7a3b2841ed096cb34c30af0fb1
Fly Dev <dev@fly.io>
2025-07-02 17:36:58 +0000

    replace home route with bins route

--- lib/smart_bins_web/router.ex
+++ lib/smart_bins_web/router.ex
@@ -17,7 +17,7 @@ defmodule SmartBinsWeb.Router do
   scope "/", SmartBinsWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", BinsLive
   end
 
   # Other scopes may use custom stacks.
